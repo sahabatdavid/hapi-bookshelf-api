eeb71db2901e0aad1fae477026f1ee007fc42326
sahabatdavid <hellosahabatdavid@gmail.com>
2021-05-04 13:43:57 +0700

    Add get books method and cors to server

--- src/handler.js
+++ src/handler.js
@@ -36,4 +36,14 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
-module.exports = addBookHandler;
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
-const addBookHandler = require('./handler');
+const {
+  addBookHandler,
+  getAllBooksHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllBooksHandler,
   },
 ];
 
